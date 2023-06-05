37c1730fa6d118ff4b796d18360c33790594cbc8
ruijie.zhang <ruijie.zhang@sincerecloud.com>
2023-06-05 10:41:08 +0800

    行长度限制

--- utils/util.go
+++ utils/util.go
@@ -11,33 +11,35 @@ import (
 	"os"
 	"strings"
 )
+
 type log struct {
 	Name string
 	Line int
 }
+
 const (
 	quit = 'm'
 )
 
-//获取指定文件阅读日志
-func ChangeLog (logPath ,fileName string,nowLine int ){
+// 获取指定文件阅读日志
+func ChangeLog(logPath, fileName string, nowLine int) {
 	f, err := ioutil.ReadFile(logPath)
 	if err != nil {
 		fmt.Println("read log fail", err.Error())
 	}
 	var logObj []log
-	err = json.Unmarshal(f,&logObj)
-	if err!=nil{
-		fmt.Println("解析日志错误 ",err)
+	err = json.Unmarshal(f, &logObj)
+	if err != nil {
+		fmt.Println("解析日志错误 ", err)
 	}
-	for k,v := range logObj{
+	for k, v := range logObj {
 		if v.Name == fileName {
 			logObj[k].Line = nowLine
 		}
 	}
-	bytedata ,_ := json.Marshal(logObj)
-	err = WriteFile(logPath,bytedata,0666)
-	if err!=nil{
+	bytedata, _ := json.Marshal(logObj)
+	err = WriteFile(logPath, bytedata, 0666)
+	if err != nil {
 		fmt.Println(err.Error())
 	}
 
@@ -58,46 +60,46 @@ func WriteFile(filename string, data []byte, perm os.FileMode) error {
 	return err
 }
 
-//获取指定文件阅读日志
-func ReadLog (logPath ,fileName string)(line int){
+// 获取指定文件阅读日志
+func ReadLog(logPath, fileName string) (line int) {
 	f, err := ioutil.ReadFile(logPath)
 	if err != nil {
 		fmt.Println("read log fail", err.Error())
 	}
 	var logObj []log
-	err = json.Unmarshal(f,&logObj)
-	if err!=nil{
-		fmt.Println("解析日志错误 ",err)
+	err = json.Unmarshal(f, &logObj)
+	if err != nil {
+		fmt.Println("解析日志错误 ", err)
 	}
 	var hasLog bool
-	for _,v := range logObj{
+	for _, v := range logObj {
 		if v.Name == fileName {
 			line = v.Line
 			hasLog = true
 		}
 	}
-	if hasLog{
+	if hasLog {
 		return line
 	}
 	var logThis = log{
-		Name:fileName,
-		Line:0,
+		Name: fileName,
+		Line: 0,
 	}
 
-	logObj = append(logObj,logThis)
-	bytedata ,_ := json.Marshal(logObj)
-	err = WriteFile(logPath,bytedata,0666)
-	if err!=nil{
+	logObj = append(logObj, logThis)
+	bytedata, _ := json.Marshal(logObj)
+	err = WriteFile(logPath, bytedata, 0666)
+	if err != nil {
 		fmt.Println(err.Error())
 	}
 
 	return
 }
 
-func validUTF8(buf []byte) bool{
+func validUTF8(buf []byte) bool {
 	nBytes := 0
-	for i:= 0;i<len(buf);i++{
-		if nBytes == 0{
+	for i := 0; i < len(buf); i++ {
+		if nBytes == 0 {
 			if (buf[i] & 0x80) != 0 {
 				for (buf[i] & 0x80) != 0 {
 					buf[i] <<= 1
@@ -108,8 +110,8 @@ func validUTF8(buf []byte) bool{
 				}
 				nBytes--
 			}
-		}else{
-			if buf[i] & 0xc0 != 0x80{
+		} else {
+			if buf[i]&0xc0 != 0x80 {
 				return false
 			}
 			nBytes--
@@ -129,10 +131,10 @@ func PathExists(path string) (bool, error) {
 	return false, err
 }
 
-func Read2(path string,goLine int,logPath string) {
+func Read2(path string, goLine int, logPath string) {
 	fi, err := os.Open(path)
 	if err != nil {
-		fmt.Println("请输入正确文件路径:",path)
+		fmt.Println("请输入正确文件路径:", path)
 		return
 	}
 	defer fi.Close()
@@ -141,33 +143,44 @@ func Read2(path string,goLine int,logPath string) {
 	r := bufio.NewReader(fi)
 	var line = 1
 	for {
-		str , err:= r.ReadString('\n')
+		str, err := r.ReadString('\n')
 		if err != nil && err != io.EOF {
-			fmt.Println("读取文件错误:",path)
+			fmt.Println("读取文件错误:", path)
 			return
 		}
-		if err == io.EOF{
+		if err == io.EOF {
 			fmt.Println("已读取到文章末尾")
 			return
 		}
-		if line < goLine{
+		if line < goLine {
 			line++
 			continue
 		}
 		str = strings.Replace(str, "\n", "", -1)
-		str = fmt.Sprintf("%d : %s",line,str)
-		if !validUTF8([]byte(str)){
-			fmt.Println(enc.ConvertString(str))
-		}else {
-			fmt.Println(str)
+		r := []rune(str)
+		l := 120
+		n := 1
+		for i := 0; i < len(r); i += l {
+			end := i + l
+			if end > len(r) {
+				end = len(r)
+			}
+			sub := r[i:end]
+			s := fmt.Sprintf("%d.%d : %s", line, n, string(sub))
+			if !validUTF8([]byte(s)) {
+				fmt.Println(enc.ConvertString(s))
+			} else {
+				fmt.Println(s)
+			}
+			n++
 		}
 		char, _, err := keyboard.GetSingleKey()
-		if (err != nil) {
+		if err != nil {
 			panic(err)
 		}
-		if char == quit || char == 46 || char == 183 || char == 96{
-			defer ChangeLog(logPath,path,line)
-			for i:=0;i<1000;i++{
+		if char == quit || char == 46 || char == 183 || char == 96 {
+			defer ChangeLog(logPath, path, line)
+			for i := 0; i < 1000; i++ {
 				fmt.Printf("\n")
 			}
 			return
